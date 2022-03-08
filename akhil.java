214977959e742e30fe4fe7a4ca4740b93412831a
pokuriakhil123 <101098391+pokuriakhil123@users.noreply.github.com>
2022-03-08 18:10:09 +0530

    Add files via upload

---
+++ akhil.java
@@ -0,0 +1,10 @@
+
+class akhil
+{
+    public static void main(String args[])
+    {
+        System.out.println("Name - Pokuri Akhil");
+        System.out.println("Collge Name - Veltech");
+        System.out.println("Branch Name - Mechanical");
+    }
+}
